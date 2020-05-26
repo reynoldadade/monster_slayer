416029bd966318e56835ab1c611c1614504c7c6b
Reyold Osei Adade <reynoldadade@gmail.com>
2020-05-26 16:58:18 +0000

    completedc alculate damage function

--- app.js
+++ app.js
@@ -13,5 +13,34 @@ new Vue({
 			this.monsterHealth = 100;
 			this.gameIsRunning = 100;
 		},
+		//this method allows the user to attack
+		attack: function () {
+			let max = 10;
+			let min = 3;
+			let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+			this.monsterHealth -= damage;
+
+			if (this.monsterHealth <= 0) {
+				alert('You won!');
+				this.gameIsRunning = false;
+				return;
+			}
+
+			max = 12;
+			min = 5;
+			damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+			this.playerHealth -= damage;
+			if (this.playerHealth <= 0) {
+				alert('You lost!');
+				this.gameIsRunning = false;
+				return;
+			}
+		},
+		//this method allows the user to use a special attack
+		specialAttack: function () {},
+		//this method allows the user to heal
+		heal: function () {},
+		//this method allows the user to giveUp
+		giveUp: function () {},
 	},
 });
